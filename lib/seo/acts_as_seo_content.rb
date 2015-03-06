d3f8704df4a758932f9104ffe01dc63d7a809f10
Piotr Niełacny <piotr.nielacny@gmail.com>
2015-03-06 11:32:02 +0100

    Update acts_as_seo_content.rb
    Do not call `after_destroy` directly on `ActiveRecord::Base` class. This will provide to exception.
    
    `ActiveRecord::Base doesn't belong in a hierarchy descending from ActiveRecord`

--- lib/seo/acts_as_seo_content.rb
+++ lib/seo/acts_as_seo_content.rb
@@ -8,19 +8,17 @@ module Seo
       @@registered_models ||= []
     end
 
-    included do
-      after_destroy -> do
-        return unless Seo::ActsAsSeoContent.registered_models.include?(self.class.to_s)
-        MetaContent.where(resource_type: self.class.to_s, resource_id: self.id).
-          delete_all
-      end
-    end
-
     module ClassMethods
       def acts_as_seo_content(options={})
         unless Seo::ActsAsSeoContent.registered_models.include?(self.to_s)
           Seo::ActsAsSeoContent.registered_models << self.to_s
         end
+        
+      after_destroy -> do
+        return unless Seo::ActsAsSeoContent.registered_models.include?(self.class.to_s)
+        MetaContent.where(resource_type: self.class.to_s, resource_id: self.id).
+          delete_all
+      end
 
         cattr_accessor :seo_resource_title
         self.seo_resource_title = (options[:seo_resource_title] || :title)
