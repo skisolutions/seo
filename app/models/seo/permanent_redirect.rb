a1cc76a5abe2584fe0c48b0f787fc38902178828
Chris Ward <chris.ward@alliants.com>
2017-09-28 13:46:40 +0100

    Remove validation for destination id to be unique

--- app/models/seo/permanent_redirect.rb
+++ app/models/seo/permanent_redirect.rb
@@ -5,7 +5,7 @@ module Seo
     attr_accessible :destination_id, :origin_id
 
     validates :origin_id, uniqueness: true, presence: true
-    validates :destination_id, uniqueness: true, presence: true
+    validates :destination_id, presence: true
     validates_with InfiniteRedirect::Validator
     validates_with CircularRedirect::Validator
   end
